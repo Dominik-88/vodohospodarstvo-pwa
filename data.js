592104be27a9fa663caf083eddec191295c3802b
free_Domek <dominikschmied4@gmail.com>
2025-12-26 07:09:47 +0100

    📊 Přidána data všech 41 areálů

---
+++ data.js
@@ -0,0 +1,419 @@
+// ============================================
+// DATA VODOHOSPODÁŘSKÝCH AREÁLŮ
+// ============================================
+// Aktualizováno: 11/2025
+// Celkem: 41 areálů
+// ============================================
+
+export const areasData = [
+  {
+    okres: 'PI',
+    nazev: 'VDJ Amerika II',
+    kategorie: 'I.',
+    oploceni: 293,
+    vymera: 3303,
+    lat: 49.305131,
+    lng: 14.166126,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.305131,14.166126'
+  },
+  {
+    okres: 'ST',
+    nazev: 'VDJ Drahonice',
+    kategorie: 'I.',
+    oploceni: 376,
+    vymera: 5953,
+    lat: 49.202902,
+    lng: 14.063713,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.202902,14.063713'
+  },
+  {
+    okres: 'ST',
+    nazev: 'VDJ Vodňany',
+    kategorie: 'I.',
+    oploceni: 252,
+    vymera: 1594,
+    lat: 49.164550,
+    lng: 14.177836,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.164550,14.177836'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Hlavatce',
+    kategorie: null,
+    oploceni: 424,
+    vymera: 7968,
+    lat: 49.063584,
+    lng: 14.267751,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.063584,14.267751'
+  },
+  {
+    okres: 'PT',
+    nazev: 'VDJ Šibeniční vrch I',
+    kategorie: 'I.',
+    oploceni: 245,
+    vymera: 1835,
+    lat: 49.025083,
+    lng: 13.994111,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.025083,13.994111'
+  },
+  {
+    okres: 'PT',
+    nazev: 'ÚV Husinecka přehrada',
+    kategorie: null,
+    oploceni: 703,
+    vymera: 4908,
+    lat: 49.034362,
+    lng: 13.996830,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.034362,13.996830'
+  },
+  {
+    okres: 'PT',
+    nazev: 'VDJ Šibeniční vrch II',
+    kategorie: 'I.',
+    oploceni: 340,
+    vymera: 3206,
+    lat: 49.026710,
+    lng: 13.994001,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.026710,13.994001'
+  },
+  {
+    okres: 'PI',
+    nazev: 'VDJ Zálužany',
+    kategorie: null,
+    oploceni: 299,
+    vymera: 2350,
+    lat: 49.552857,
+    lng: 14.083381,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.552857,14.083381'
+  },
+  {
+    okres: 'PT',
+    nazev: 'VDJ Ptáčník',
+    kategorie: 'II.',
+    oploceni: 239,
+    vymera: 1070,
+    lat: 49.066147,
+    lng: 14.186844,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.066147,14.186844'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Zdoba',
+    kategorie: 'II.',
+    oploceni: 225,
+    vymera: 15523,
+    lat: 49.212422,
+    lng: 14.338095,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.212422,14.338095'
+  },
+  {
+    okres: 'CK',
+    nazev: 'VDJ Domoradice',
+    kategorie: 'I.',
+    oploceni: 450,
+    vymera: 4148,
+    lat: 48.829651,
+    lng: 14.326564,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.829651,14.326564'
+  },
+  {
+    okres: 'CK',
+    nazev: 'VDJ Horní Brána',
+    kategorie: 'I.',
+    oploceni: 187,
+    vymera: 1665,
+    lat: 48.807970,
+    lng: 14.329352,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.807970,14.329352'
+  },
+  {
+    okres: 'CK',
+    nazev: 'VDJ Netřebice',
+    kategorie: 'I.',
+    oploceni: 136,
+    vymera: 877,
+    lat: 48.783277,
+    lng: 14.456447,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.783277,14.456447'
+  },
+  {
+    okres: 'CK',
+    nazev: 'VDJ Plešivec',
+    kategorie: 'I.',
+    oploceni: 119,
+    vymera: 975,
+    lat: 48.802231,
+    lng: 14.304933,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.802231,14.304933'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Doudleby',
+    kategorie: 'II.',
+    oploceni: 79,
+    vymera: 413,
+    lat: 48.888896,
+    lng: 14.480271,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.888896,14.480271'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Jankov',
+    kategorie: 'I.',
+    oploceni: 106,
+    vymera: 784,
+    lat: 48.968747,
+    lng: 14.301697,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.968747,14.301697'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Hosín II',
+    kategorie: 'I.',
+    oploceni: 399,
+    vymera: 4173,
+    lat: 49.030641,
+    lng: 14.501012,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.030641,14.501012'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Chlum',
+    kategorie: 'II.',
+    oploceni: 63,
+    vymera: 535,
+    lat: 49.096493,
+    lng: 14.388679,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.096493,14.388679'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Chotýčany',
+    kategorie: 'II.',
+    oploceni: 338,
+    vymera: 4775,
+    lat: 49.070748,
+    lng: 14.519460,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.070748,14.519460'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Rudolfov III',
+    kategorie: 'I.',
+    oploceni: 174,
+    vymera: 1868,
+    lat: 48.986207,
+    lng: 14.547076,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.986207,14.547076'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Rimov - Vesce',
+    kategorie: 'I.',
+    oploceni: 99,
+    vymera: 662,
+    lat: 48.847847,
+    lng: 14.466957,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.847847,14.466957'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Hosin',
+    kategorie: 'II.',
+    oploceni: 125,
+    vymera: 809,
+    lat: 49.033641,
+    lng: 14.492878,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.033641,14.492878'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Včelná',
+    kategorie: 'II.',
+    oploceni: 476,
+    vymera: 8660,
+    lat: 48.924663,
+    lng: 14.463506,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.924663,14.463506'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Húry',
+    kategorie: 'I.',
+    oploceni: 0,
+    vymera: 395,
+    lat: 49.006417,
+    lng: 14.549815,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.006417,14.549815'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Chlumec',
+    kategorie: 'II.',
+    oploceni: 110,
+    vymera: 811,
+    lat: 49.124766,
+    lng: 14.431321,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.124766,14.431321'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Olešník',
+    kategorie: 'I.',
+    oploceni: 117,
+    vymera: 380,
+    lat: 49.111103,
+    lng: 14.377766,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.111103,14.377766'
+  },
+  {
+    okres: 'CB',
+    nazev: 'ČS Bukovec',
+    kategorie: 'I.',
+    oploceni: 300,
+    vymera: 4943,
+    lat: 48.881608,
+    lng: 14.449233,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.881608,14.449233'
+  },
+  {
+    okres: 'CB',
+    nazev: 'VDJ Heřman',
+    kategorie: 'II.',
+    oploceni: 119,
+    vymera: 982,
+    lat: 48.909479,
+    lng: 14.499876,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.909479,14.499876'
+  },
+  {
+    okres: 'CB',
+    nazev: 'ČS Vidov u řeky',
+    kategorie: 'II.',
+    oploceni: 212,
+    vymera: 2501,
+    lat: 48.924157,
+    lng: 14.489619,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.924157,14.489619'
+  },
+  {
+    okres: 'CB',
+    nazev: 'Vrt Vidov',
+    kategorie: 'II.',
+    oploceni: 164,
+    vymera: 470,
+    lat: 48.924066,
+    lng: 14.489679,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.924066,14.489679'
+  },
+  {
+    okres: 'CB',
+    nazev: 'ÚV Plav',
+    kategorie: 'I.',
+    oploceni: 1413,
+    vymera: 74777,
+    lat: 48.912611,
+    lng: 14.494018,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=48.912611,14.494018'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Čekanice',
+    kategorie: 'I.',
+    oploceni: 450,
+    vymera: 6344,
+    lat: 49.422197,
+    lng: 14.689896,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.422197,14.689896'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Svatá Anna',
+    kategorie: 'I.',
+    oploceni: 264,
+    vymera: 4192,
+    lat: 49.401133,
+    lng: 14.698640,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.401133,14.698640'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Bezděčín',
+    kategorie: 'I.',
+    oploceni: 169,
+    vymera: 1996,
+    lat: 49.323096,
+    lng: 14.628405,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.323096,14.628405'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Milevsko',
+    kategorie: 'I.',
+    oploceni: 129,
+    vymera: 823,
+    lat: 49.452521,
+    lng: 14.344102,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.452521,14.344102'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Hodušín',
+    kategorie: 'II.',
+    oploceni: 205,
+    vymera: 1708,
+    lat: 49.429670,
+    lng: 14.474214,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.429670,14.474214'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Všechov',
+    kategorie: 'I.',
+    oploceni: 199,
+    vymera: 1574,
+    lat: 49.430159,
+    lng: 14.623205,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.430159,14.623205'
+  },
+  {
+    okres: 'TA',
+    nazev: 'VDJ Zlukov',
+    kategorie: 'II.',
+    oploceni: 184,
+    vymera: 1520,
+    lat: 49.196289,
+    lng: 14.736382,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.196289,14.736382'
+  },
+  {
+    okres: 'TA',
+    nazev: 'ÚV Tábor',
+    kategorie: 'II.',
+    oploceni: 350,
+    vymera: 12262,
+    lat: 49.422872,
+    lng: 14.666426,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.422872,14.666426'
+  },
+  {
+    okres: 'TA',
+    nazev: 'ČS Sudoměřice',
+    kategorie: 'I.',
+    oploceni: 220,
+    vymera: 2508,
+    lat: 49.286580,
+    lng: 14.547794,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.286580,14.547794'
+  },
+  {
+    okres: 'TA',
+    nazev: 'Provozní Vodojem Tábor',
+    kategorie: 'II.',
+    oploceni: 155,
+    vymera: 1853,
+    lat: 49.424264,
+    lng: 14.666384,
+    mapUrl: 'https://www.google.com/maps/search/?api=1&query=49.424264,14.666384'
+  }
+];
